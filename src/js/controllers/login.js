b3f030398b7dfcf102ffb8c616563684e6fc686d
arnif <arnifa@gmail.com>
2014-02-17 19:44:36 +0000

    chat alot

--- src/js/controllers/login.js
+++ src/js/controllers/login.js
@@ -4,16 +4,30 @@ app.controller("LoginController", ["$scope", "$location", "SocketService", funct
 	var socket = io.connect('http://localhost:8080');
 
 	$scope.connect = function() {
+
 		if(socket) {
+			if ($scope.username === "") {
+				$(".error-msg").fadeIn();
+					$scope.message = "Name can't empty";
+					setTimeout(function () {
+						$(".error-msg").fadeOut();
+					},5500);
+					return;
+			}
 			socket.emit("adduser", $scope.username, function(available) {
 				if(available) {
+					$("#blackout").fadeOut();
 					SocketService.setConnected(socket);
 					SocketService.setUsername($scope.username);
 
 					$location.path("/room/lobby");
 				}
 				else {
+					$(".error-msg").fadeIn();
 					$scope.message = "Your name is taken, please choose another";
+					setTimeout(function () {
+						$(".error-msg").fadeOut();
+					},5500);
 				}
 				$scope.$apply();
 			});
--- src/js/controllers/room.js
+++ src/js/controllers/room.js
@@ -1,4 +1,4 @@
-app.controller("RoomController", ["$scope", "$routeParams", "SocketService", function($scope, $routeParams, SocketService) {
+app.controller("RoomController", ["$scope", "$routeParams", "$location", "SocketService", function($scope, $routeParams, $location, SocketService) {
 	$scope.roomName = $routeParams.roomName;
 	$scope.currentMessage = "";
 
@@ -9,10 +9,14 @@ app.controller("RoomController", ["$scope", "$routeParams", "SocketService", fun
 
 		});
 
+
 		socket.on("updatechat", function(roomname, messageHistory) {
 			console.log(messageHistory);
 			$scope.messages = messageHistory;
 			$scope.$apply();
+			var wholeHeight = $(".chat-window")[0].scrollHeight;
+			$(".chat-window").scrollTop(wholeHeight);
+		
 		});
 
 		socket.on("updateusers", function(room, users) {
@@ -34,5 +38,23 @@ app.controller("RoomController", ["$scope", "$routeParams", "SocketService", fun
 		if($event.keyCode === 13) {
 			$scope.send();
 		}
+	};
+
+	$scope.createRoom = function() {
+		console.log("NEW ROOM");
+		$("#blackout").fadeIn();
+		$("#create-room").fadeIn();
+
+		
+
+		$scope.create = function() {
+			var n = $("#room-name").val();
+			console.log("name " +n);
+			$("#blackout").fadeOut();
+			$("#create-room").fadeOut();
+			$location.path("/room/" + n);
+		};
+
+
 	};
 }]);
